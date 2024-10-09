21e864049aa44f7b747c08e6176b4744999e8cdd
EvanPerreau <evan.perreau@gmail.com>
2024-10-09 15:21:40 +0200

    chore : initialize main

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+require_once 'Helpers/Psr4AutoloaderClass.php';
+
+$loader = new \Helpers\Psr4AutoloaderClass;
+
+$loader->register();
+$loader->addNamespace('\Helpers', '/Helpers');
+$loader->addNamespace('\League\Plates\\', '/Vendor/Plates/src');
+
+$engine = new \League\Plates\Engine('Views');
+echo $engine->render('home', ['tftSetName' => 'Set 3']);
