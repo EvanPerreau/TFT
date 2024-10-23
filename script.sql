ad9c1e19de3ba27de704691e89109b5f24ad64ad
EvanPerreau <evan.perreau@gmail.com>
2024-10-23 16:56:50 +0200

    feat(sql): add unit to database on the script.sql

--- script.sql
+++ script.sql
@@ -5,3 +5,6 @@ CREATE TABLE UNIT (
   origin VARCHAR(255) NOT NULL,
   url_img VARCHAR(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO UNIT (id, name, cost, origin, url_img) VALUES ('1', 'Swordman', 1, 'Barracks', 'https://vignette.wikia.nocookie.net/ageofempires/images/4/4b/SwordsmanDE.png/revision/latest/scale-to-width-down/340?cb=20200820183157');
+INSERT INTO UNIT (id, name, cost, origin, url_img) VALUES ('2', 'Archer', 2, 'Archery Range', 'https://vignette.wikia.nocookie.net/ageofempires/images/4/4b/SwordsmanDE.png/revision/latest/scale-to-width-down/340?cb=20200820183157');
